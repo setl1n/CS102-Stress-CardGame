82da7a2d24ab975c5e9eab88fcceaec2a5ee659f
Rui Cong Kwan <ruicong@Ruis-MacBook-Air.local>
2024-03-11 15:28:36 +0800

    ammended isValidThrow in gamelogic

--- src/Game/GameLogicUtils.java
+++ src/Game/GameLogicUtils.java
@@ -1,19 +1,15 @@
 package Game;
 
 import Collections.DeckComponents.Card;
+import Collections.DeckComponents.CardComponents.*;;
 
 public class GameLogicUtils {
+    
     public static boolean isValidThrow(Card card1, Card card2){
-        System.out.println("card1 symbol: " + card1.getRank().getSymbol());
-        System.out.println("card2 symbol: " + card2.getRank().getSymbol());
-        String card1Rank = card1.getRank().getSymbol();
-        String card2Rank = card2.getRank().getSymbol();
-        int card1RankAsInt = getInt(card1Rank);
-        int card2RankAsInt = getInt(card2Rank);
-        if ((card1RankAsInt == 1 && card2RankAsInt == 13) || (card1RankAsInt == 13 && card2RankAsInt == 1)) {
-            return true;
-        }
-        return Math.abs(card1RankAsInt - card2RankAsInt) <= 1;
+        Rank card1Rank = card1.getRank();
+        Rank card2Rank = card2.getRank();
+        int difference = Math.abs(card1Rank.compareTo(card2Rank));
+        return (difference == 13 || difference == 0 || difference == 1);
     }
 
     public static int getInt(String symbol) {
