2eddcea4f0a485eb676c9ae63c65b555b6ca80fb
Rui Cong Kwan <ruicong@Ruis-MacBook-Air.local>
2024-03-29 10:05:31 +0800

    refactored controls

--- src/Controls/MainControls.java
+++ src/Controls/MainControls.java
@@ -26,68 +26,84 @@ public class MainControls extends KeyAdapter {
     public void keyPressed(KeyEvent e) {
         int newKeyPress = e.getKeyCode();
         pressedKeys.add(newKeyPress);
-        Player player1 = game.getPlayer1();
-        Player player2 = game.getPlayer2();
-        Pile[] piles = game.getBothPiles();
         switch (game.getGameState()) {
-            // start screen
             case START_SCREEN:
-                if (newKeyPress == KeyEvent.VK_SPACE) {
-                    GUI.changeToPanel("Game");
-                    SoundUtility.bgmSound();
-                    game.setGameState(GameState.OPEN_FIRST_CARDS);
-                }
+                handleStartScreenKeyPress(newKeyPress);
                 break;
             case OPEN_FIRST_CARDS:
-                if (pressedKeys.contains(KeyEvent.VK_S) && pressedKeys.contains(KeyEvent.VK_K)) {
-                    game.openCardsFromDeck();
-                    game.setGameState(GameState.PLAYING);
-                }
+                handleOpenFirstCardsKeyPress();
                 break;
-            // standard gameplay conditions
             case PLAYING:
-                switch (newKeyPress) {
-                    case KeyEvent.VK_Q -> player1.throwCardToPile(0, piles);
-                    case KeyEvent.VK_W -> player1.throwCardToPile(1, piles);
-                    case KeyEvent.VK_E -> player1.throwCardToPile(2, piles);
-                    case KeyEvent.VK_R -> player1.throwCardToPile(3, piles);
-                    case KeyEvent.VK_A -> player1.setTargetPileIndex(0);
-                    case KeyEvent.VK_D -> player1.setTargetPileIndex(1);
-                    case KeyEvent.VK_S -> game.stress(player2);
-                    case KeyEvent.VK_U -> player2.throwCardToPile(0, piles);
-                    case KeyEvent.VK_I -> player2.throwCardToPile(1, piles);
-                    case KeyEvent.VK_O -> player2.throwCardToPile(2, piles);
-                    case KeyEvent.VK_P -> player2.throwCardToPile(3, piles);
-                    case KeyEvent.VK_J -> player2.setTargetPileIndex(0);
-                    case KeyEvent.VK_L -> player2.setTargetPileIndex(1);
-                    case KeyEvent.VK_K -> game.stress(player1);
-                }
+                handlePlayingKeyPress(newKeyPress);
                 break;
-            // freeze screen conditions
             case STRESS, NO_VALID_MOVES:
-                switch (newKeyPress) {
-                    case KeyEvent.VK_A -> player1.setTargetPileIndex(0);
-                    case KeyEvent.VK_D -> player1.setTargetPileIndex(1);
-                    case KeyEvent.VK_J -> player2.setTargetPileIndex(0);
-                    case KeyEvent.VK_L -> player2.setTargetPileIndex(1);
-                }
-                if (pressedKeys.contains(KeyEvent.VK_S) && pressedKeys.contains(KeyEvent.VK_K)) {
-                    game.openCardsFromDeck();
-                    game.setGameState(GameState.PLAYING);
-                }
-                // play stress cut screen, lock
+                handleStressNoValidMovesKeyPress(newKeyPress);
                 break;
-            // end game conditions
             case STALEMATE, PLAYER1_WINS, PLAYER2_WINS:
-                System.out.println("END CONDITION DETECTED");
-                switch (newKeyPress) {
-                    case KeyEvent.VK_PERIOD -> GUI.dispose();
-                }
+                handleEndGameKeyPress(newKeyPress);
                 break;
         }
         System.out.println("Keys Pressed: " + pressedKeys.stream().map(i -> (char) i.intValue()).collect(Collectors.toList()));
         System.out.println(game);
         game.updateGameState();
+    }
+
+    private void handleStartScreenKeyPress(int newKeyPress) {
+        if (newKeyPress == KeyEvent.VK_SPACE) {
+            GUI.changeToPanel("Game");
+            SoundUtility.bgmSound();
+            game.setGameState(GameState.OPEN_FIRST_CARDS);
+        }
+    }
+
+    private void handleOpenFirstCardsKeyPress() {
+        if (pressedKeys.contains(KeyEvent.VK_S) && pressedKeys.contains(KeyEvent.VK_K)) {
+            game.openCardsFromDeck();
+            game.setGameState(GameState.PLAYING);
+        }
+    }
+
+    private void handlePlayingKeyPress(int newKeyPress) {
+        Player player1 = game.getPlayer1();
+        Player player2 = game.getPlayer2();
+        Pile[] piles = game.getBothPiles();
+        switch (newKeyPress) {
+            case KeyEvent.VK_Q -> player1.throwCardToPile(0, piles);
+            case KeyEvent.VK_W -> player1.throwCardToPile(1, piles);
+            case KeyEvent.VK_E -> player1.throwCardToPile(2, piles);
+            case KeyEvent.VK_R -> player1.throwCardToPile(3, piles);
+            case KeyEvent.VK_A -> player1.setTargetPileIndex(0);
+            case KeyEvent.VK_D -> player1.setTargetPileIndex(1);
+            case KeyEvent.VK_S -> game.stress(player2);
+            case KeyEvent.VK_U -> player2.throwCardToPile(0, piles);
+            case KeyEvent.VK_I -> player2.throwCardToPile(1, piles);
+            case KeyEvent.VK_O -> player2.throwCardToPile(2, piles);
+            case KeyEvent.VK_P -> player2.throwCardToPile(3, piles);
+            case KeyEvent.VK_J -> player2.setTargetPileIndex(0);
+            case KeyEvent.VK_L -> player2.setTargetPileIndex(1);
+            case KeyEvent.VK_K -> game.stress(player1);
+        }
+    }
+
+    private void handleStressNoValidMovesKeyPress(int newKeyPress) {
+        Player player1 = game.getPlayer1();
+        Player player2 = game.getPlayer2();
+        switch (newKeyPress) {
+            case KeyEvent.VK_A -> player1.setTargetPileIndex(0);
+            case KeyEvent.VK_D -> player1.setTargetPileIndex(1);
+            case KeyEvent.VK_J -> player2.setTargetPileIndex(0);
+            case KeyEvent.VK_L -> player2.setTargetPileIndex(1);
+        }
+        if (pressedKeys.contains(KeyEvent.VK_S) && pressedKeys.contains(KeyEvent.VK_K)) {
+            game.openCardsFromDeck();
+            game.setGameState(GameState.PLAYING);
+        }
+    }
+
+    private void handleEndGameKeyPress(int newKeyPress) {
+        if (newKeyPress == KeyEvent.VK_PERIOD) {
+            GUI.dispose();
+        }
     }
 
     @Override
