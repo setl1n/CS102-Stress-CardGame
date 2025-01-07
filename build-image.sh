0335c873846cffcd3a514fbbdf4b41d6f5eec7c9
setl1n <kaungsetlin2008@gmail.com>
2025-01-07 21:27:40 +0800

    resource file loading and build script for all resources

--- build-image.sh
+++ build-image.sh
@@ -1,14 +1,27 @@
 native-image \
   -H:+UnlockExperimentalVMOptions \
   --initialize-at-build-time=com.sun.media.sound \
-  -H:+AddAllCharsets \
-  -H:EnableURLProtocols=resource \
+  --initialize-at-build-time=javax.sound.sampled.AudioFormat \
+  --initialize-at-build-time=com.sun.media.sound.JSSecurityManager \
+  --initialize-at-run-time=com.sun.media.sound.DirectAudioDevice \
+  --initialize-at-run-time=javax.sound.sampled.AudioSystem \
+  -H:+AllowIncompleteClasspath \
+  -H:EnableURLProtocols=resource,file \
   -H:+ReportExceptionStackTraces \
   --enable-monitoring \
-  --initialize-at-run-time=com.sun.media.sound.DirectAudioDevice \
-  -H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.AudioFileReader \
-  --initialize-at-run-time=javax.sound.sampled.AudioSystem \
   -Djava.awt.headless=false \
-  -H:ConfigurationFileDirectories=./META-INF/native-image/ -H:IncludeResources="./assets/*" \
+  -H:ConfigurationFileDirectories=./META-INF/native-image/ \
+  -H:IncludeResources=".*/assets/.*" \
+  -H:IncludeResources=".*\\.properties" \
+  -H:IncludeResources="META-INF/services/.*" \
+  --enable-url-protocols=resource,file \
+  -H:+PrintClassInitialization \
   -jar target/cs102-cardgame-1.0-SNAPSHOT.jar stress
 # native-image  -H:+UnlockExperimentalVMOptions -H:Name=StressCardGame -H:ConfigurationFileDirectories=./META-INF/native-image/ -H:IncludeResources="./assets/*" -jar target/cs102-cardgame-1.0-SNAPSHOT.jar stress
+
+#   -H:ServiceLoaderFeatureConfiguration=javax.sound.sampled.spi.AudioFileReader:serviceloader \
+#   -H:ServiceLoaderFeatureConfiguration=javax.sound.sampled.spi.FormatConversionProvider:serviceloader \
+#   -H:ServiceLoaderFeatureConfiguration=javax.sound.sampled.spi.MixerProvider:serviceloader \
+#   -H:+TraceServiceLoaderFeature \
+#   --initialize-at-run-time=com.sun.media.sound.JSSecurityManager \
+#   -Djava.home="." \
