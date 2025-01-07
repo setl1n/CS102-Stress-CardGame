5a24de9e80352a105cc16123e18300f1da6f3903
setl1n <kaungsetlin2008@gmail.com>
2025-01-07 23:06:00 +0800

    overloading build script

--- build-image.sh
+++ build-image.sh
@@ -1,10 +1,12 @@
 native-image \
   -H:+UnlockExperimentalVMOptions \
+  --initialize-at-build-time=java.desktop \
   --initialize-at-build-time=com.sun.media.sound \
-  --initialize-at-build-time=javax.sound.sampled.AudioFormat \
-  --initialize-at-build-time=com.sun.media.sound.JSSecurityManager \
+  --initialize-at-build-time=javax.sound.sampled,com.sun.media.sound \
   --initialize-at-run-time=com.sun.media.sound.DirectAudioDevice \
+  --initialize-at-run-time=com.sun.media.sound.PortMixer \
   --initialize-at-run-time=javax.sound.sampled.AudioSystem \
+  --initialize-at-run-time=com.sun.media.sound.JSSecurityManager \
   -H:+AllowIncompleteClasspath \
   -H:EnableURLProtocols=resource,file \
   -H:+ReportExceptionStackTraces \
@@ -16,6 +18,7 @@ native-image \
   -H:IncludeResources="META-INF/services/.*" \
   --enable-url-protocols=resource,file \
   -H:+PrintClassInitialization \
+  --enable-all-security-services \
   -jar target/cs102-cardgame-1.0-SNAPSHOT.jar stress
 # native-image  -H:+UnlockExperimentalVMOptions -H:Name=StressCardGame -H:ConfigurationFileDirectories=./META-INF/native-image/ -H:IncludeResources="./assets/*" -jar target/cs102-cardgame-1.0-SNAPSHOT.jar stress
 
@@ -23,5 +26,4 @@ native-image \
 #   -H:ServiceLoaderFeatureConfiguration=javax.sound.sampled.spi.FormatConversionProvider:serviceloader \
 #   -H:ServiceLoaderFeatureConfiguration=javax.sound.sampled.spi.MixerProvider:serviceloader \
 #   -H:+TraceServiceLoaderFeature \
-#   --initialize-at-run-time=com.sun.media.sound.JSSecurityManager \
 #   -Djava.home="." \
